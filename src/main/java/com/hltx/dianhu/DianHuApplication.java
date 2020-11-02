7c222732e3cded2c04905899f5774f78ee1e0405
yangchangjiang <1872567027@qq.com>
2020-11-02 13:25:29 +0800

    Initial commit

---
+++ src/main/java/com/hltx/dianhu/DianHuApplication.java
@@ -0,0 +1,16 @@
+package com.hltx.dianhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 项目启动类
+ * @author: ycj
+ * @create: 2020-11-02 12:11
+ **/
+@SpringBootApplication
+public class DianHuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DianHuApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hltx/dianhu/beans/Users.java
@@ -0,0 +1,92 @@
+package com.hltx.dianhu.beans;
+
+import java.util.Date;
+
+/**
+ * @description: 用户实体类
+ * @author: ycj
+ * @create: 2020-11-02 13:19
+ **/
+public class Users {
+
+    private Integer id;
+
+    private String phone;
+
+    private String password;
+
+    private String Sn;
+
+    private Integer status;
+
+    private Date addtime;
+
+    private Date endtime;
+
+    private Date startTime;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSn() {
+        return Sn;
+    }
+
+    public void setSn(String sn) {
+        Sn = sn;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Date addtime) {
+        this.addtime = addtime;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+}
